16d445a3565584fcc9d5f10e190cfa5964574a2d
Robin Hogan <r.j.hogan.ecmwf.int>
2023-08-31 09:28:59 +0000

    Updated Matlab files to match revised version of paper

--- plot/evaluate_quadrature.m
+++ plot/evaluate_quadrature.m
@@ -1,3 +1,7 @@
+% This Matlab script plots the simulations previously performed in the
+% "run" directory, producing some of the figures 3 and 4 of the Hogan
+% paper
+
 directory='../run';
 dataset='evaluation2';
 scenario='present';
@@ -5,11 +9,23 @@ supspace = ' '; % Fix bad PDF rendering in some Matlab installations
 input_file = [directory '/optical-depth/ecckd-1.0_' dataset '_lw_climate_fsck-32b_optical-depth_' scenario '.nc'];
 ref_file = [directory '/fluxes/' dataset '_gauss-jacobi-5_64_fluxes.nc'];
 quadratures = {'gauss-legendre','gauss-laguerre','gauss-jacobi-5',...
-	       'optimized-default','elsasser','lacis'};
-is_ratio=[0 0 0 0 0 0];
-is_one  =[0 0 0 0 1 3];
-leg = {'Gauss-Legendre','Gauss-Laguerre','Gauss-Jacobi 5','Optimized',...
-       'Elsasser','Lacis'};
+	       'optimized-default','optimized-default-ratio','elsasser','lacis'};
+% Legends
+leg_full = {'Gauss-Legendre','Gauss-Laguerre','Gauss-Jacobi-5','Optimized',...
+       'Optimized-IR','Elsasser','Lacis & Oinas'}
+leg = {'Legendre','Laguerre','Jacobi-5','Optimized',...
+       'Optimized-IR','Elsasser','Lacis'}
+stys = {'-','-','-','-','--','-','-'};
+colours = [0 0 0;
+	   0 0 1;
+	   1 0 0;
+	   0 0.7 0;
+	   0.3 1 0;
+	   1 0 1;
+	   0 0.9 0.9];
+reorder = 1:length(quadratures);
+show_hr_error_bar = [1 0 1 0 0 0 0];
+streams_offset = [0 0 0 0 1 0 2];
 
 in = loadnc(input_file);
 ref= loadnc(ref_file);
@@ -17,7 +33,6 @@ ref= loadnc(ref_file);
 nlay = size(ref.heating_rate,1);
 hr_weight = (sqrt(in.pressure_hl(2:end,:))-sqrt(in.pressure_hl(1:end-1,:))) ...
 	    ./ (ones(nlay,1)*sqrt(in.pressure_hl(end,:)));
-%hr_weight(find(in.pressure_hl(2:end,:)<100e2))=0.0;
 pmid = 0.5.*(in.pressure_hl(2:end,:)+in.pressure_hl(1:end-1,:));
 % Tropospheric weight
 index = find(pmid<100e2);
@@ -31,20 +46,19 @@ hr_weight_strat(index) = 0.0;
 hr_weight_strat = hr_weight_strat./(ones(nlay,1)*sum(hr_weight_strat));
 
 for iquad = 1:length(quadratures)
-  if is_ratio(iquad)
-    orders = 2;
-  elseif is_one(iquad)
-    orders = is_one(iquad);
+  qdata = loadnc([directory '/quadrature/quadrature_' quadratures{iquad} '.nc']);
+  orders=double(qdata.order(find(qdata.order<=16)));
+  if length(orders)>1
+    is_one(iquad) = 0;
+    if orders(1) > 1
+      syms{iquad} = '*--';
+    else
+      syms{iquad} = 'o-';
+    end
   else
-    orders = [1:8 10 12 14 16];
-%    orders = 1:4;
+    is_one(iquad) = orders(1);
+    syms{iquad} = '*';
   end
-  %if strcmp(quadratures{iquad}, 'optimized')
-  %  orders = [1:8];
-  %elseif strcmp(quadratures{iquad}(1:12), 'optimized-fw')
-  %  orders = [1:4];
-  %else
-  %end
   streams{iquad} = orders.*2;
   for iord = 1:length(orders)
     file = [directory '/fluxes/' dataset '_' quadratures{iquad} '_' ...
@@ -75,16 +89,15 @@ for iquad = 1:length(quadratures)
   tot_errstd{iquad}  = sqrt(flux_weight.*(toa_errvar{iquad}+surf_errvar{iquad}) ...
 			    +hr_errvar{iquad});
 end
-cols = {'k','b','r','g','m','c'};
 
+% Figure 3 of the Hogan paper
 figure(1)
 clf
-%set(gcf,'paperposition',[0.5 0.5 30 10],'defaultlinelinewidth',1)
 set(gcf,'paperposition',[0.5 0.5 27 20],'defaultlinelinewidth',1)
 
 subplot(2,3,1)
 for iquad = 1:length(quadratures)
-  loglog(streams{iquad}, flux_errstd{iquad}, [cols{iquad} 'o-']);
+  loglog(streams{iquad}, flux_errstd{iquad}, syms{iquad},'color',colours(iquad,:));
   hold on
 end
 xlabel('Number of streams')
@@ -95,95 +108,96 @@ set(gca,'xtick',[2 4 8 16 32],'xminortick','off','xminorgrid','off');
 set(gca,'yminortick','on','yminorgrid','off')
 grid on
 legend(leg,'location','southwest')
-text(0,1.01,'\bf(a)','units','normalized','verticalalignment','bottom')
+text(0,1.01,'\bf(a) Irradiances','units','normalized','verticalalignment','bottom')
 
 subplot(2,3,2)
 for iquad = 1:length(quadratures)
-  %loglog(streams{iquad}, hr_errstd{iquad}, [cols{iquad} 'o-']);
-  loglog(streams{iquad}, hr_errstd_trop{iquad}, [cols{iquad} 'o-']);
+  loglog(streams{iquad}, hr_errstd_trop{iquad}, syms{iquad},'color',colours(iquad,:));
   hold on
 end
 xlabel('Number of streams')
-ylabel(['RMSE in tropospheric heating rate (K d^{' supspace '-1})'])
+ylabel(['RMSE in heating rate,  {\itp} >100 hPa (K d^{' supspace '-1})'])
 xlim([1.8 36])
 ylim([1e-8 1e-1]);set(gca,'ytick',10.^[-8:1])
 set(gca,'xtick',[2 4 8 16 32],'xminortick','off','xminorgrid','off');
 set(gca,'yminortick','on','yminorgrid','off')
 grid on
-text(0,1.01,'\bf(b)','units','normalized','verticalalignment','bottom')
-%subplot(2,2,3)
-%for iquad = 1:length(quadratures)
-%  loglog(streams{iquad}, surf_errstd{iquad}, [cols{iquad} 'o-']);
-%  hold on
-%end
+text(0,1.01,'\bf(b) Tropospheric heating rates','units','normalized','verticalalignment','bottom')
 
 subplot(2,3,3)
 for iquad = 1:length(quadratures)
-  %loglog(streams{iquad}, tot_errstd{iquad}, [cols{iquad} 'o-']);
-  loglog(streams{iquad}, hr_errstd_strat{iquad}, [cols{iquad} 'o-']);
+  loglog(streams{iquad}, hr_errstd_strat{iquad}, syms{iquad},'color',colours(iquad,:));
   hold on
 end
 xlabel('Number of streams')
-%ylabel('Combined RMSE')
-ylabel(['RMSE in stratospheric heating rate (K d^{' supspace '-1})'])
+ylabel(['RMSE in heating rate,  {\itp} <100 hPa (K d^{' supspace '-1})'])
 xlim([1.8 36])
 set(gca,'xtick',[2 4 8 16 32],'xminortick','off','xminorgrid','off');
 set(gca,'yminortick','on','yminorgrid','off')
 grid on
-text(0,1.01,'\bf(c)','units','normalized','verticalalignment','bottom')
+text(0,1.01,'\bf(c) Stratospheric heating rates','units','normalized','verticalalignment','bottom')
 ylim([1e-7 1e0]);set(gca,'ytick',10.^[-8:1])
-%figure(2)
-%clf
-%pp=median(0.5.*(in.pressure_hl(1:end-1,:)+in.pressure_hl(2:end,:)),2)./100;
-%for iquad = 1:length(quadratures)
-%  semilogy(hr_errvar_lay{iquad}(:,2),pp,[cols{iquad} 'o-'])
-%  hold on
-%end
+
+% For graphical abstract
+figure(10)
+clf
+set(gcf,'paperposition',[0.5 0.5 27 20],'defaultlinelinewidth',1)
+
+subplot(2,3,1)
+for iquad = 1:length(quadratures)
+  loglog(streams{iquad}, flux_errstd{iquad}, syms{iquad},'color',colours(iquad,:));
+  hold on
+end
+xlabel('Number of streams')
+ylabel(['RMS error in clear-sky fluxes (W m^{' supspace '-2})'])
+xlim([1.8 36])
+ylim([1e-7 1e1]);set(gca,'ytick',10.^[-8:1])
+set(gca,'xtick',[2 4 8 16 32],'xminortick','off','xminorgrid','off');
+set(gca,'yminortick','on','yminorgrid','off')
+grid on
+pos=get(gca,'position')
+legend(leg_full,'location','eastoutside')
+drawnow
+set(gca,'position',pos);
+
 
 pmidmed = median(pmid,2)./100;
 
-gg=[0.5 0.5 0.5;
-    0.25 0.25 1;
-    1 0.25 0.25;
-    0.25 1 0.25
-    1 0.25 1];
 gg=[0.25 0.25 0.25;
     0 0 1;
     1 0 0;
     0 1 0;
     0 1 1];
-cols = {'k','b','r','g','m--','c--','y'};
 
+% Figure 4 of the Hogan paper
 figure(2)
 clf
 set(gcf,'paperposition',[0.5 0.5 32 10]);
 xspan = [1 0.2 0.05];
-reorder = [1 3 4 2 5 6];
 set(gcf,'defaultlinelinewidth',1.5);
 for io = 1:3
-  subplot(1,3,io)
+  h=subplot(1,3,io);
+  pos=get(h,'position');
+  set(h,'position',pos+[0 0.05 0 -0.05]);
   if io == 1
     for iq=1:length(bias_hr)
-      plot(-1,-1,cols{iq})
+      plot(-1,-1,stys{iq},'color',colours(iq,:))
       hold on
     end
   end
   for iq = 1:length(bias_hr)
     iqq = reorder(iq);
-    if iqq < 5
+    if show_hr_error_bar(iqq)
       h=fill([bias_hr{iqq}(:,io)-ci_hr{iqq}(:,io);...
 	      flip(bias_hr{iqq}(:,io)+ci_hr{iqq}(:,io))],...
-	     [pmidmed;flip(pmidmed)],'r','facecolor',gg(iqq,:),'edgecolor','none');%cols{iqq});
+	     [pmidmed;flip(pmidmed)],'r','facecolor',gg(iqq,:),'edgecolor','none');
       set(h,'facealpha',0.2);
       hold on
-    elseif ~is_one(iqq) | io==is_one(iqq)
-%      plot(bias_hr{iqq}(:,io)-ci_hr{iqq}(:,io),pmidmed,'k--','color',gg(iqq,:),'linewidth',0.5)
-%      plot(bias_hr{iqq}(:,io)+ci_hr{iqq}(:,io),pmidmed,'k--','color',gg(iqq,:),'linewidth',0.5)
     end
   end
    for iq = 1:length(bias_hr)
     iqq = reorder(iq);
-    if ~is_one(iqq)
+    if ~is_one(iqq) & show_hr_error_bar(iqq)
       plot(bias_hr{iqq}(:,io)-ci_hr{iqq}(:,io),pmidmed,'k:','color',gg(iqq,:),'linewidth',0.5)
       plot(bias_hr{iqq}(:,io)+ci_hr{iqq}(:,io),pmidmed,'k:','color',gg(iqq,:),'linewidth',0.5)
     end
@@ -192,9 +206,12 @@ for io = 1:3
     iqq = reorder(iq);
     if ~is_one(iqq) | io==is_one(iqq)
       if is_one(iqq)
-	semilogy(bias_hr{iqq}(:,1),pmidmed,cols{iqq});
+	semilogy(bias_hr{iqq}(:,1),pmidmed,stys{iqq},'color',colours(iqq,:));
       else
-	semilogy(bias_hr{iqq}(:,io),pmidmed,cols{iqq});
+	ind=io-streams_offset(iqq);
+	if ind > 0
+	  semilogy(bias_hr{iqq}(:,io-streams_offset(iqq)),pmidmed,stys{iqq},'color',colours(iqq,:));
+	end
       end
     end
   end
@@ -204,10 +221,10 @@ for io = 1:3
   set(gca,'xtick',xspan(io).*[-1 -0.5 0 0.5 1])
   grid on
   plot([0 0],[1e-2 1e3], 'k','linewidth',0.5)
-  xlabel('Heating rate bias (K/d)');
+  xlabel('Heating rate bias (K d^{-1})');
   ylabel('Pressure (hPa)')
   if io == 1
-    legend('Legendre','Laguerre','Jacobi 5','Optimized','Elsasser','Lacis','location','southeast')
+    legend(leg,'location','southeast')
   end
   set(gca,'yminorgrid','off','layer','top')
   set(gca,'Xticklabelrotationmode','manual')
--- plot/print_quadrature.m
+++ plot/print_quadrature.m
@@ -1,21 +1,69 @@
 % Print out numbers for paper
 qstr = {'gauss-legendre','gauss-laguerre','gauss-jacobi-5','optimized-fw0.02'};
+qstr = {'gauss-legendre','gauss-laguerre','gauss-jacobi-5','optimized-default','optimized-default-ratio2'};
+qstr = {'gauss-laguerre','gauss-jacobi-5','optimized-default','optimized-default-ratio2','optimized-default-jacobi-5-ratio-prior3'};
+
+transform_weights = 1;
+
 for iq = 1:length(qstr)
-  qdat{iq} = loadnc(['../run/quadrature_' qstr{iq} '.nc']);
+  qdat{iq} = loadnc(['../run/quadrature/quadrature_' qstr{iq} '.nc']);
+
+  if transform_weights
+    for iorder = 1:length(qdat{iq}.order)
+      wt = qdat{iq}.weight(1:qdat{iq}.order(iorder),iorder).*qdat{iq}.mu(1:qdat{iq}.order(iorder),iorder);
+      wt = wt./sum(wt);
+      qdat{iq}.weight(1:qdat{iq}.order(iorder),iorder) = wt;      
+    end
+  end
+end
+qdat{4}.mu=[[0;0;0;0] qdat{4}.mu];
+qdat{4}.weight=[[0;0;0;0] qdat{4}.weight];
+qdat{5}.mu=[[0;0;0;0] qdat{5}.mu];
+qdat{5}.weight=[[0;0;0;0] qdat{5}.weight];
+
+% To compare Gauss-Jacobi-5 to Table 25.8 of Abramowitz and Stegun
+%qdat{2}.weight = qdat{2}.weight./6;
+
+do_row_colour = 1;
+do_ratio = 1;
+
+precision = '%10.8f';
+precision = '%12.10f';
+
+if do_row_colour
+  disp('\hline');
 end
 
 for io = 1:4
   prefix = [num2str(io) ' & $\mu$ &'];
   for in = 1:io
-    disp(sprintf('%s %10.8f & %10.8f & %10.8f & %10.8f \\\\', ...
-		prefix, qdat{1}.mu(in,io), qdat{2}.mu(in,io), qdat{3}.mu(in,io), qdat{4}.mu(in,io)));
+    if io == 1
+      mustr4='';
+      mustr5='';
+    elseif do_ratio & in > 1
+      mustr4=sprintf([precision ' ($\\times$%d)'],qdat{4}.mu(in,io),round(qdat{4}.mu(in,io)./qdat{4}.mu(1,io)));
+      mustr5=sprintf([precision ' ($\\times$%d)'],qdat{5}.mu(in,io),round(qdat{5}.mu(in,io)./qdat{5}.mu(1,io)));
+    else
+      mustr4=sprintf(precision,qdat{4}.mu(in,io));
+      mustr5=sprintf(precision,qdat{5}.mu(in,io));
+    end
+    if do_row_colour
+      disp('\rowcolor{white}');
+    end
+    disp(sprintf(['%s ' precision ' & ' precision ' & ' precision ' & %s & %s \\\\'], ...
+		 prefix, qdat{1}.mu(in,io), qdat{2}.mu(in,io), qdat{3}.mu(in,io), mustr4, mustr5));
     prefix = '  &       &';
   end
   prefix = '  & $w$   &';
   for in = 1:io
-    disp(sprintf('%s %10.8f & %10.8f & %10.8f & %10.8f \\\\',...
-		prefix, qdat{1}.weight(in,io), qdat{2}.weight(in,io), qdat{3}.weight(in,io), qdat{4}.weight(in,io)));
+    if do_row_colour
+      disp('\rowcolor{black!10}');
+    end
+    disp(sprintf(['%s ' precision ' & ' precision ' & ' precision ' & ' precision ' & ' precision ' \\\\'],...
+		prefix, qdat{1}.weight(in,io), qdat{2}.weight(in,io), qdat{3}.weight(in,io), qdat{4}.weight(in,io), qdat{5}.weight(in,io)));
     prefix = '  &       &';
   end
-
+  if do_row_colour
+    disp('\hline');
+  end
 end
